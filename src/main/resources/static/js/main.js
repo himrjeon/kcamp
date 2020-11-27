fa73a9493af47fddfe2b72fe4a06e6c49f65c3ce
CSLab1228 <chief_hi@naver.com>
2020-11-27 16:02:06 +0900

    네이버 애널리틱스 연동

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,5 @@
 console.log('%c FrontEnd Proudly Crafted with ZiOn.', 'background: #222; color: #bada55');
-console.log('%c BackEnd Proudly Crafted with CSLab.', 'background: #222; color: #bada55');
+console.log('%c BackEnd Proudly Crafted with HI.', 'background: #222; color: #bada55');
 
 /* ---------------------------------------------- /*
  * Preloader
